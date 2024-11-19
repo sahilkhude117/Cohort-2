efce157482f49c90ece0ee44a71c8317afb28be4
sahilkhude117 <sahilkhude11@gmail.com>
2024-11-19 12:45:29 +0530

    react-basics

--- week-4/classroom/vite-project/src/App.jsx
+++ week-4/classroom/vite-project/src/App.jsx
@@ -1,16 +1,42 @@
-import { useState } from 'react'
-import './App.css'
+/* eslint-disable no-unused-vars */
+import React from "react";
+import useState from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+  const [todos,setTodos] = useState([{
+    title: "Go to Gym",
+    description: "Go to Gym from 7-9",
+    completed: false,
+  }, {
+    title: "dsa",
+    description: " from 7-9",
+    completed: true,
+  }]);
+
+  function addTodo(){
+    setTodos([...todos, {
+      title: "new Todo",
+      description: "desc to new todo"
+    }])
+  }
 
   return (
     <div>
-      <button onClick={()=>setCount((count)=>count+1)}>
-        count is {count}
-      </button>
+      <button onClick={addTodo}>Add a TODO</button>
+      {todos.map((todo) => {
+        // eslint-disable-next-line react/jsx-key
+        return <Todo title={todo.title} description={todo.description}/>
+      })}
     </div>
   )
 }
 
+function Todo(props){
+  return <div>
+    <h1>props.title</h1>
+    <h2>props.description</h2>
+    </div>
+}
+
 export default App
+
